bbdc0cc6cf7ae7939e59533084a5b88a7200840c
eskbak <116146600+eskbak@users.noreply.github.com>
2022-10-22 16:38:25 +0200

    Add files via upload

--- GPS_kode.ino
+++ GPS_kode.ino
@@ -1,20 +1,21 @@
 #include "TinyGPS++.h"
-#include "SoftwareSerial.h"
 
-SoftwareSerial serial_connection(3, 2); //RX=pin 10, TX=pin 11
+#define RXD2 16
+#define TXD2 17
+
 TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
 void setup()
 {
   Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
-  serial_connection.begin(9600);//This opens up communications to the GPS
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);//This opens up communications to the GPS
   Serial.println("GPS Start");//Just show to the monitor that the sketch has started
 }
 
 void loop()
 {
-  while(serial_connection.available())//While there are characters to come from the GPS
+  while(Serial2.available())//While there are characters to come from the GPS
   {
-    gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
+    gps.encode(Serial2.read());//This feeds the serial NMEA data into the library one char at a time
   }
   if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
   {
